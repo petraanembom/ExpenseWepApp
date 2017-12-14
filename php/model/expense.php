eeade598c44d84cc945ab7feb4a3c72860f99584
prestech <muwanpresley@gmail.com>
2017-12-13 21:04:24 -0500

    added data object models for user, expense and household

---
+++ php/model/expense.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+* 
+*/
+class Expense
+{
+	private var exp_id;
+	private var category;
+	private var amount;
+	private var date_of_entry;
+	private var date_incurred;
+	private var user_id;
+	private var household_id; 
+	private var expense_type; //personal or household
+	
+	function __construct(argument)
+	{
+		# code...
+	}
+
+
+	function getId(){
+
+	}
+	
+
+
+}
+?>
---
+++ php/model/user.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* 
+*/
+class User
+{
+	private var username;
+	private var lastname;
+	private var firstname;
+	private var email;
+	private var house_id;
+	private var user_id;
+	
+	function __construct(argument)
+	{
+		# code...
+	}
+}//user class ends 
+
+?>
