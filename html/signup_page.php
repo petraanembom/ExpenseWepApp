27ec981b8fac1df59f20a13ffcc2d01c08288b12
prestech <muwanpresley@gmail.com>
2017-12-14 20:45:06 -0500

    added coloring to the views

--- html/signup_page.php
+++ html/signup_page.php
@@ -14,56 +14,81 @@
      require $_SERVER['DOCUMENT_ROOT']."/env_params/php_path_constants.php";
 	?>
 
+	<style type="text/css">
+		#sign_up_form{
+			background: yellow;
+			display: table;
+		}
+
+		#sign_up_fieldset{
+			
+		}
+		#sign_up_fieldset p{
+			display: table-row;
+		}
+		#sign_up_form input{
+			display: table-cell;
+			margin: 10px;
+		}
+
+	</style>
+
 </head>
 
 <!--HTML BODY-->
 <body>
 
+
 	<div>
 		<div>
 
-			 <h3> Enter all fields to create and account </h3>
-
 			<!--implement javascript to validate all form information-->
-			<form action=<?php echo SINGUP_SERVICE ?> method="post">
-				<div>
-				<input type="text" name="firstname" placeholder="FIRST NAME"> 
-				</div>
+			<!--TODO: Enforce checks to make sure user fills in this form with the right and required information--> 
+			<form id="sign_up_form" action=<?php echo SINGUP_SERVICE ?> method="post">
 
-				<div>
-					<input type="text" name="lastname" placeholder="LAST NAME"> 
-				</div>
+				<fieldset id="sign_up_fieldset">
+					<legend> Create user account </legend>
+					<p>
+					<input type="text" name="firstname" placeholder="FIRST NAME"> 
+					</p>
 
-				<div>
-				 	<input type="text" name="username" placeholder="USERNAME"> 
-				 </div>
+					<p>
+						
+						<input type="text" name="lastname" placeholder="LAST NAME"> 
+					</p>
 
-				<!--div>
-					<input type="text" name="data_of_birth" placeholder="DATE OF BIRTH">
-				</div-->
+					<p>
+						
+					 	<input type="text" name="username" placeholder="USERNAME"> 
+					 </p>
+
+					<!--div>
+						<input type="text" name="data_of_birth" placeholder="DATE OF BIRTH">
+					</div-->
+					
+					<p>
+						
+						<input type="email" name="email" placeholder="EMAIL">
+					</p>
+
+					<p>
+						
+						<input type="password" name="password" placeholder="PASSWORD"> 
+					</p>
+
+					<p>
+						
+						<input type="password" name="re-password" placeholder="RE-PASSWORD">
+					</p>
+
+					<p>
+						<input type="submit" value="Register">
+						<input id="cancel_btn" type="button" value="cancel" onclick= "window.location.href='<?php echo INDEX_PAGE ?>' "> 
+					</p>
 				
-				<div>
-					<input type="email" name="email" placeholder="EMAIL">
-				</div>
-
-				<div>
-					<input type="password" name="password" placeholder="PASSWORD"> 
-				</div>
-
-				<div>
-					<input type="password" name="re-password" placeholder="RE-PASSWORD">
-				</div>
-
-				<div>
-					<input type="submit">
-				</div>
+			    </fieldset>
 				
 			</form>
-
-			<!--link to cancel this process of account creation
-			 *When clicked a pop-out window should be displayed
-			 * asking the user to confirm the cancel process-->
-			<a href=<?php echo INDEX_PAGE ?> > Cancel </a>
 
 		</div>
 	</div>
